6a014dd0caf6130b5e8005f9a3787248a3d7b144
Francisco Souza <francisco@franciscosouza.net>
2010-06-27 21:43:10 -0300

    Defined second step

--- school/features/step_definitions/students_registry_steps.py
+++ school/features/step_definitions/students_registry_steps.py
@@ -17,6 +17,21 @@ def clear_students(scenario):
 def usernamefy(name):
     return slugify(name).replace('-', '_')
 
+def do_login():
+    world.browser.get(django_url('/admin/login/'))
+    dom = world.browser.get_dom()
+
+    needs_login = bool(dom.cssselect('body.login,form#login-form,#id_username,#id_password'))
+    if needs_login:
+        username = world.browser.find_element_by_selector("#id_username")
+        password = world.browser.find_element_by_selector("#id_password")
+        submit = world.browser.find_element_by_selector("input[value='Log in']")
+
+        first_user = world.users[0]
+        username.send_keys(first_user.username)
+        password.send_keys(first_user.password)
+        submit.click()
+
 @step(u'Given there is an user "(.*)" with the password "(.*)"')
 def given_there_is_an_user_group1_with_the_password_group2(step, username, password):
     name = username
@@ -34,3 +49,8 @@ def given_there_is_an_user_group1_with_the_password_group2(step, username, passw
 
     data = type('UserData', (), locals())
     world.users.append(data)
+
+@step(u'When I navigate to the new student page')
+def when_i_navigate_to_the_new_student_page(step):
+    do_login()
+    world.browser.get('/school/students/new')
