28ebb3b2ef508a60786ba20edfec332382f38c5d
Francisco Souza <francisco@franciscosouza.net>
2010-07-07 20:44:03 -0300

    Changed URL mapping

--- school/urls.py
+++ school/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('school.views',
-    url(r'^students/new', 'new_student', name = 'new_students_page'),
+    url(r'^students/new$', 'new_student', name = 'new_students_page'),
 )
