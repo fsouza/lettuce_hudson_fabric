c363ebbcc16242348e488510547c957da6a28bd1
Francisco Souza <francisco@franciscosouza.net>
2010-07-07 23:11:21 -0300

    Made the fifth step definition works (or not, a little problem with unicode that I will check later)

--- school/urls.py
+++ school/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('school.views',
     url(r'^students/new$', 'new_student', name = 'new_students_page'),
+    url(r'^students/view/(?P<student_id>\d+)$', 'show_student', name = 'view_student_page'),
 )
--- school/views.py
+++ school/views.py
@@ -1,11 +1,24 @@
 # Create your views here.
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from forms import StudentForm
+from models import Student
 
 def new_student(request):
     form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            student = form.save()
+            return HttpResponseRedirect(reverse('view_student_page', args=[student.pk]))
     return render_to_response('form_new_student.html', {
-            'form' : form 
+            'form' : form
         }, context_instance=RequestContext(request)
     )
+
+def show_student(request, student_id):
+    student = Student.objects.get(pk = student_id)
+    return HttpResponse('Hi!')
